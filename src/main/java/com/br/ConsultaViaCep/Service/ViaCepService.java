c11adf16ab5c36192ced4b1ad3ba041ccac276a4
leonardodecastrodasilveira <dev.leonardocastro@gmail.com>
2023-06-20 08:47:50 -0300

    Objeto convertido em json

--- src/main/java/com/br/ConsultaViaCep/Service/ViaCepService.java
+++ src/main/java/com/br/ConsultaViaCep/Service/ViaCepService.java
@@ -1,52 +1,50 @@
 package com.br.ConsultaViaCep.Service;
+
 import com.br.ConsultaViaCep.Model.EnderecoModel;
 import com.google.gson.Gson;
-
 import org.springframework.stereotype.Service;
 
-import java.io.*;
 import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Arrays;
 
 @Service
 public class ViaCepService {
 
-    public EnderecoModel executaConsultaNoSiteDosCorreios(String cepQueSeraConsultado)  {
+    public EnderecoModel executaConsultaNoSiteDosCorreios(String cepQueSeraConsultado) {
         EnderecoModel enderecoModel = new EnderecoModel();
 
-            String endPoint = "https://viacep.com.br/ws/"+cepQueSeraConsultado+"/json/";
-            HttpRequest httpRequest = HttpRequest.newBuilder()
-                    .uri(URI.create(endPoint))
-                    .build();
-            HttpClient httpClient = HttpClient.newBuilder().build();
-            HttpResponse<String> httpResponse = null;
-            try {
-                httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-            } catch (Exception e) {
-            }
-            //TODO converter o objeto que esta chegando para o EnderecoModel
+        String endPoint = "https://viacep.com.br/ws/" + cepQueSeraConsultado + "/json/";
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(endPoint))
+                .build();
+        HttpClient httpClient = HttpClient.newBuilder().build();
+        HttpResponse<String> httpResponse = null;
+        try {
+            httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+        }
+        //TODO converter o objeto que esta chegando para o EnderecoModel (done)
 
 
+        EnderecoModel endereco = new Gson().fromJson(httpResponse.body(), EnderecoModel.class);
 
+//            var endereco1 = new EnderecoModel();
+//        BeanUtils.copyProperties(httpResponse, enderecoModel);
 
-//        EnderecoModel endereco = new Gson().fromJson(httpResponse.body(),EnderecoModel.class);
-//
-//        enderecoModel.setCep(endereco.getCep());
-//        enderecoModel.setLogradouro(endereco.getLogradouro());
-//        enderecoModel.setComlemento(endereco.getComlemento());
-//        enderecoModel.setBairro(endereco.getBairro());
-//        enderecoModel.setLocalidade(endereco.getLocalidade());
-//        enderecoModel.setUf(endereco.getUf());
-//        enderecoModel.setIbge(endereco.getIbge());
-//        enderecoModel.setGia(endereco.getGia());
-//        enderecoModel.setDdd(endereco.getDdd());
-//        enderecoModel.setSiafi(endereco.getSiafi());
+        enderecoModel.setCep(endereco.getCep());
+        enderecoModel.setLogradouro(endereco.getLogradouro());
+        enderecoModel.setComlemento(endereco.getComlemento());
+        enderecoModel.setBairro(endereco.getBairro());
+        enderecoModel.setLocalidade(endereco.getLocalidade());
+        enderecoModel.setUf(endereco.getUf());
+        enderecoModel.setIbge(endereco.getIbge());
+        enderecoModel.setGia(endereco.getGia());
+        enderecoModel.setDdd(endereco.getDdd());
+        enderecoModel.setSiafi(endereco.getSiafi());
 
         return enderecoModel;
+//        return endereco1;
     }
 }
